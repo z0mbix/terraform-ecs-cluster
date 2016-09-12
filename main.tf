f2f750c6fb03518a8a27e924c994cc12d0d89d1c
David Wooldridge (z0mbix) <zombie@zombix.org>
2016-09-12 20:45:56 +0100

    terraform fmt

--- main.tf
+++ main.tf
@@ -5,79 +5,78 @@ resource "aws_ecs_cluster" "name" {
 
 # Create a security group
 resource "aws_security_group" "ecs" {
-    name = "ecs_${var.environment}"
-    description = "Allows all access from VPC and SSH from office"
-    vpc_id = "${lookup(var.vpc_id, var.environment)}"
+  name        = "ecs_${var.environment}"
+  description = "Allows all access from VPC and SSH from office"
+  vpc_id      = "${lookup(var.vpc_id, var.environment)}"
 
-    # Allow SSH access from everywhere (Update this if required)
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  # Allow SSH access from everywhere (Update this if required)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    # Allow access from VPC
-    ingress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["${split(",", lookup(var.vpc_cidr_blocks, var.environment))}"]
-    }
+  # Allow access from VPC
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["${split(",", lookup(var.vpc_cidr_blocks, var.environment))}"]
+  }
 
-    # Allow all outgoing traffic
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  # Allow all outgoing traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 # Create a launch configuration for the ECS EC2 instances
 resource "aws_launch_configuration" "ecs" {
-    name = "ecs-${lookup(var.cluster_name, var.environment)}"
-    image_id = "${lookup(var.ami, var.region)}"
-    instance_type = "${var.instance_type}"
-    iam_instance_profile = "${lookup(var.iam_instance_profile, var.environment)}"
-    key_name = "${var.key_name}"
-    security_groups = [ "${aws_security_group.ecs.id}" ]
-    user_data = "#!/bin/bash\nyum install -y -q aws-cli && aws s3 cp s3://ecs/ecs.config.template /etc/ecs/ecs.config.template && sed \"s/__CLUSTERNAME__/${lookup(var.cluster_name, var.environment)}/g\" /etc/ecs/ecs.config.template > /etc/ecs/ecs.config"
+  name                 = "ecs-${lookup(var.cluster_name, var.environment)}"
+  image_id             = "${lookup(var.ami, var.region)}"
+  instance_type        = "${var.instance_type}"
+  iam_instance_profile = "${lookup(var.iam_instance_profile, var.environment)}"
+  key_name             = "${var.key_name}"
+  security_groups      = ["${aws_security_group.ecs.id}"]
+  user_data            = "#!/bin/bash\nyum install -y -q aws-cli && aws s3 cp s3://ecs/ecs.config.template /etc/ecs/ecs.config.template && sed \"s/__CLUSTERNAME__/${lookup(var.cluster_name, var.environment)}/g\" /etc/ecs/ecs.config.template > /etc/ecs/ecs.config"
 
-    root_block_device = {
-      volume_type = "gp2"
-      volume_size = "${var.volume_size}"
-    }
+  root_block_device = {
+    volume_type = "gp2"
+    volume_size = "${var.volume_size}"
+  }
 }
 
 # Create an auto-scaling group for the EC2 ECS instances
 resource "aws_autoscaling_group" "ecs-cluster" {
-  availability_zones = ["${split(",", var.availability_zones)}"]
-  vpc_zone_identifier = ["${split(",", lookup(var.subnet_ids, var.environment))}"]
-  name = "ecs-${lookup(var.cluster_name, var.environment)}"
-  min_size = "${var.min_size}"
-  max_size = "${var.max_size}"
-  desired_capacity = "${var.desired_capacity}"
-  health_check_type = "EC2"
-  launch_configuration = "${aws_launch_configuration.ecs.name}"
+  availability_zones        = ["${split(",", var.availability_zones)}"]
+  vpc_zone_identifier       = ["${split(",", lookup(var.subnet_ids, var.environment))}"]
+  name                      = "ecs-${lookup(var.cluster_name, var.environment)}"
+  min_size                  = "${var.min_size}"
+  max_size                  = "${var.max_size}"
+  desired_capacity          = "${var.desired_capacity}"
+  health_check_type         = "EC2"
+  launch_configuration      = "${aws_launch_configuration.ecs.name}"
   health_check_grace_period = "${var.health_check_grace_period}"
 
   tag {
-    key = "environment"
-    value = "${var.environment}"
+    key                 = "environment"
+    value               = "${var.environment}"
     propagate_at_launch = true
   }
 
   tag {
-    key = "Name"
-    value =  "${lookup(var.instance_name_prefix, var.environment)}"
+    key                 = "Name"
+    value               = "${lookup(var.instance_name_prefix, var.environment)}"
     propagate_at_launch = true
   }
 
   tag {
-    key = "role"
-    value =  "ecs"
+    key                 = "role"
+    value               = "ecs"
     propagate_at_launch = true
   }
 }
-
--- variables.tf
+++ variables.tf
@@ -1,125 +1,131 @@
 # The environment (production, staging, qa etc.)
 variable "environment" {
-    description = "Environment name to tag EC2 resources with (tag=environment)"
+  description = "Environment name to tag EC2 resources with (tag=environment)"
 }
 
 # AWS EC2 Region
 variable "region" {
-    default = "eu-west-1"
-    description = "The AWS region"
+  default     = "eu-west-1"
+  description = "The AWS region"
 }
 
 # What to call the ECS instances
 variable "instance_name_prefix" {
-    description = "Prefix for the EC2 instance names"
-    default = {
-        production = "prod-ecs"
-        staging = "stg-ecs"
-        qa = "qa-ecs"
-    }
+  description = "Prefix for the EC2 instance names"
+
+  default = {
+    production = "prod-ecs"
+    staging    = "stg-ecs"
+    qa         = "qa-ecs"
+  }
 }
 
 # The ECS Cluster name
 variable "cluster_name" {
-    description = "The name of the ECS Cluster"
-    default = {
-        production = "production"
-        staging = "staging"
-        qa = "qa"
-    }
+  description = "The name of the ECS Cluster"
+
+  default = {
+    production = "production"
+    staging    = "staging"
+    qa         = "qa"
+  }
 }
 
 # Which AMI to launch (AWS ECS AMIs)
 variable "ami" {
-    description = "Base AMI to launch the instances with"
-    default = {
-        us-east-1 = "ami-cb2305a1"
-        us-west-1 = "ami-bdafdbdd"
-        us-west-2 = "ami-ec75908c"
-        eu-west-1 = "ami-13f84d60"
-        eu-central-1 = "ami-c3253caf"
-        ap-northeast-1 = "ami-e9724c87"
-        ap-southeast-1 = "ami-5f31fd3c"
-        ap-southeast-2 = "ami-83af8ae0"
-    }
+  description = "Base AMI to launch the instances with"
+
+  default = {
+    us-east-1      = "ami-cb2305a1"
+    us-west-1      = "ami-bdafdbdd"
+    us-west-2      = "ami-ec75908c"
+    eu-west-1      = "ami-13f84d60"
+    eu-central-1   = "ami-c3253caf"
+    ap-northeast-1 = "ami-e9724c87"
+    ap-southeast-1 = "ami-5f31fd3c"
+    ap-southeast-2 = "ami-83af8ae0"
+  }
 }
 
 # SSH key name to use
 variable "key_name" {
-    default = "ops"
-    description = "SSH key name in your AWS account for AWS instances."
+  default     = "ops"
+  description = "SSH key name in your AWS account for AWS instances."
 }
 
 # IAM Instance profile
 variable "iam_instance_profile" {
-    description = "IAM Profile to use"
-    default = {
-        production = "ecs_production"
-        staging = "ecs_staging"
-        qa = "ecs_qa"
-    }
+  description = "IAM Profile to use"
+
+  default = {
+    production = "ecs_production"
+    staging    = "ecs_staging"
+    qa         = "ecs_qa"
+  }
 }
 
 # Availability zones to launch instances in to
 variable "availability_zones" {
-    default = "eu-west-1a,eu-west-1b"
-    description = "Comma separated list of EC2 availability zones to launch instances, must be within region"
+  default     = "eu-west-1a,eu-west-1b"
+  description = "Comma separated list of EC2 availability zones to launch instances, must be within region"
 }
 
 # VPC IDs
 variable "vpc_id" {
-    default = {
-        production = "vpc-XXXXXX"
-        staging = "vpc-XXXXXXXX"
-        qa = "vpc-XXXXXXXX"
-    }
-    description = "VPC ID"
+  default = {
+    production = "vpc-XXXXXX"
+    staging    = "vpc-XXXXXXXX"
+    qa         = "vpc-XXXXXXXX"
+  }
+
+  description = "VPC ID"
 }
 
 # Comma separated list of subnets in the VPC to place instances
 variable "subnet_ids" {
-    default = {
-        production = "subnet-XXXXXXXX,subnet-XXXXXXXX"
-        staging = "subnet-XXXXXXXX,subnet-XXXXXXXX"
-        qa = "subnet-XXXXXXXX,subnet-XXXXXXXX"
-    }
-    description = "Comma separated list of subnet ids, must match availability zones"
+  default = {
+    production = "subnet-XXXXXXXX,subnet-XXXXXXXX"
+    staging    = "subnet-XXXXXXXX,subnet-XXXXXXXX"
+    qa         = "subnet-XXXXXXXX,subnet-XXXXXXXX"
+  }
+
+  description = "Comma separated list of subnet ids, must match availability zones"
 }
 
 # Comma separated list of all cidr blocks for the VPC
 variable "vpc_cidr_blocks" {
-    description = "CIDR block to allow access for this VPC"
-    default = {
-        production = "10.0.0.0/16"
-        staging = "10.10.0.0/16"
-        qa = "10.20.0.0/16"
-   }
+  description = "CIDR block to allow access for this VPC"
+
+  default = {
+    production = "10.0.0.0/16"
+    staging    = "10.10.0.0/16"
+    qa         = "10.20.0.0/16"
+  }
 }
 
 variable "instance_type" {
-    description = "Name of the AWS instance type"
+  description = "Name of the AWS instance type"
 }
 
 variable "volume_size" {
-    description = "The size of the volume for an instance"
+  description = "The size of the volume for an instance"
 }
 
 variable "min_size" {
-    default = "1"
-    description = "Minimum number of instances to run in the group"
+  default     = "1"
+  description = "Minimum number of instances to run in the group"
 }
 
 variable "max_size" {
-    default = "10"
-    description = "Maximum number of instances to run in the group"
+  default     = "10"
+  description = "Maximum number of instances to run in the group"
 }
 
 variable "desired_capacity" {
-    description = "Desired number of instances to run in the group"
+  description = "Desired number of instances to run in the group"
 }
 
 variable "health_check_grace_period" {
-    default = "300"
-    description = "Time after instance comes into service before checking health"
+  default     = "300"
+  description = "Time after instance comes into service before checking health"
 }
-
